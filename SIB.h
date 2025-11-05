37afedd30837b73b401619216dd1dd4a5633c2f8
István <sibalinistvan22@gmail.com>
2025-11-05 23:16:51 +0100

    Added a new header which will include types like vectors, matrices, etc.

--- SIB.h
+++ SIB.h
@@ -1,6 +1,8 @@
 #ifndef	SIB_H
 #define	SIB_H
 
+#include	"SIB_types.h"
+
 void print_signature();
 
 #endif
---
+++ SIB_types.h
@@ -0,0 +1,12 @@
+#ifndef	SIB_TYPES_H
+#define	SIB_TYPES_H
+
+// 3 dimensional vector
+typedef struct
+{
+	float x;
+	float y;
+	float z;
+} vec3;
+
+#endif
