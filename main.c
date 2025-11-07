5cd69c819e47159e6eb4b443ebfbe1bafe70d245
István <sibalinistvan22@gmail.com>
2025-11-07 22:33:44 +0100

    added random number generation, draws a 64x64 square

--- main.c
+++ main.c
@@ -1,17 +1,36 @@
 #include	"SIB.h"
+#include	<time.h>
 
 // initializing the rectangle
 SDL_Rect rect;
 
 int main()
 {
+	srand(time(NULL));
+	
+	int r = rand();
+
+	printf("random number = %d\n",r);
+	int rSize = 32;
+	int random_numbers[(rSize*rSize)];
+
+	for (int i=0; i<(rSize*rSize); i++)
+	{
+		random_numbers[i] = r%256;
+		printf("%d\n", r%256);
+		r = rand();
+	}	
+	printf("size of random numbers = %d\n", (int)sizeof(random_numbers));
+	
+	r = rand()%256;
+
 	Engine engine =	{NULL, NULL};
 
 	// defining the rectangle
-	rect.w = 50;
-	rect.h = 50;
-	rect.x = 100;
-	rect.y = 50;
+	rect.w = 10;
+	rect.h = rect.w;
+	rect.x = 40;
+	rect.y = 40;
 	
 	// defining a triangle
 	vec3 point1 = {50, 50, 0};
@@ -57,16 +76,32 @@ int main()
 		// set the background color to "black"
 		SDL_SetRenderDrawColor(engine.renderer, 0, 0, 0, 255);	
 		SDL_RenderClear(engine.renderer);
+
+		// draw a gray-scale square. random color.	
+		for (int i=0; i<rSize; i++)
+		{
+			for (int j=0; j<rSize; j++)
+			{
 	
-		// set the color to red, and draw a rectangle
-		
-		//SDL_RenderDrawRect(engine.renderer, &rect);
-		//SDL_RenderDrawLine(engine.renderer, 50, 50, 300, 300);
+				SDL_SetRenderDrawColor(engine.renderer, random_numbers[(rSize*i)+j],
+								random_numbers[(rSize*i)+j],
+								random_numbers[(rSize*i)+j],
+								 255);
+				SDL_RenderFillRect(engine.renderer, &rect);
+				rect.x += 10;	
+			}
+			rect.y += 10;
+			rect.x = 40;
+		}	
+
 		draw_triangle(engine, tri, 255, 0, 0, 255);		
 		// render everything
 		SDL_RenderPresent(engine.renderer);
 
 		SDL_Delay(16);
+
+		rect.x = 40;
+		rect.y = 40;
 	}
 
 	game_cleanup(&engine, EXIT_SUCCESS);	
